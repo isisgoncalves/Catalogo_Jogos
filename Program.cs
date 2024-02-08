11270c99b109c5faf41bb55dd450cd273d433ff0
isisgoncalves <isisvow@gmail.com>
2024-02-08 08:11:11 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,13 +4,11 @@ class Program
 {
     static void Main()
     {
-       
         int quantidadeJogos = int.Parse(Console.ReadLine());
-
         
-       string[] nomesJogos = new string[quantidadeJogos];
+        string[] nomesJogos = new string[quantidadeJogos];
 
-       for(int i = 0; i < quantidadeJogos; i++)
+        for(int i = 0; i < quantidadeJogos; i++)
         {
           AdicionarJogo(i, nomesJogos);
         }
